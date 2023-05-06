1beae060741272f3be854b6f5ac6b717151abf88
cosmopoliitans <elfkyn52@gmail.com>
2023-05-07 01:12:52 +0300

    aydınlık mod

--- web-sayfam/src/components/Footer.js
+++ web-sayfam/src/components/Footer.js
@@ -1,5 +1,34 @@
 import React from "react";
 
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <h3 className="letsWork">
+        Let's work together on
+        <br />
+        your next product.
+      </h3>
+      <div className="rectangle">
+        <a className="email" href="mailto:elfkyn52@gmail.com">
+          <img src=".\icons\el.svg" />
+          <p className="emailcss">elfkyn52@gmail.com</p>
+        </a>
+
+        <nav className="sagF">
+          <a className="sagF1" href="">
+            Personal Blog
+          </a>
+          <a className="sagF2" href="https://github.com/cosmopoliitans">
+            GitHub
+          </a>
+          <a
+            className="sagF3"
+            href="https://www.linkedin.com/in/elif-koyun-3ba895154/"
+          >
+            Linkedin
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
 }
--- web-sayfam/src/components/Projects.js
+++ web-sayfam/src/components/Projects.js
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Projects() {
-  return <div></div>;
+  return (
+    <div className="projects">
+      <span className="profileLine"></span>
+      <h3>Projects</h3>
+      <div className="kartlarGrup">
+        <div className="projects-list">
+          <img src=".\icons\Rectangle 41.png" />
+          <h4>Workintech</h4>
+          <p className="aciklama">
+            A simple, customizable, minimal setup cookie plugin that allows your
+            users to select which cookies to accept or decline. This was created
+            with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
+            package and the git repository makes any type of customization to
+            code and themes
+          </p>
+          <div className="techStack">
+            <p>react</p>
+            <p>redux</p>
+            <p>axios</p>
+          </div>
+          <div className="details">
+            <p>GitHub</p>
+            <p>Viev Site</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
